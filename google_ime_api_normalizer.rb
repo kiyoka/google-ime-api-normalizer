5b4635767c5e6eb0eb9afca7c62a36e3f64e945b
Kiyoka Nishiyama <kiyoka@sumibi.org>
2010-12-23 00:11:53 +0900

    first version.

--- google_ime_api_normalizer.rb
+++ google_ime_api_normalizer.rb
@@ -1,25 +1,44 @@
-# $ ruby google_ime_api_normalizer.rb
+# -*- coding: utf-8 -*-
 #
 require 'rubygems'
 require 'sinatra'
+require 'uri'
+require 'net/http'
+
+class GoogleImeApi
+  def call( langpair, text )
+    langpair = "" unless langpair
+    text     = "" unless text
+
+    Net::HTTP.version_1_2
+    body = ""
+    if (0 < langpair.size) and (0 < text.size)
+      Net::HTTP.start('www.google.com', 80) {|http|
+        path = sprintf( '/transliterate?langpair=%s&text=%s', langpair, URI.encode( text ))
+        puts path
+        response = http.get(path)
+        body = response.body
+      }
+      body
+    else
+      "[]"
+    end
+  end
+
+  def normalize( jsonStr )
+    jsonStr.gsub( /\n/, "" ).gsub( /,\]/, "]" )
+  end
+end
+
 
 configure :production do
   # Configure stuff here you'll want to
   # only be run at Heroku at boot
 end
 
-# Quick test
-get '/' do
-  "Congradulations!
-   You're running a Sinatra application on Heroku!"
-end
-
-# Test at <appname>.heroku.com
-
-# You can see all your app specific information this way.
-# IMPORTANT! This is a very bad thing to do for a production
-# application with sensitive information
-
-get '/env' do
-  ENV.inspect
+# translate
+get '/transliterate' do
+  ime = GoogleImeApi.new
+  jsonStr = ime.call( params[:langpair], params[:text] )
+  ime.normalize( jsonStr )
 end
